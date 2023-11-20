4b0f5bfdcbd646508474d0624fecc38f6c961f24
Shunichi Shinohara <shino.shun@gmail.com>
2023-11-20 09:44:36 +0900

    200 OK

--- httpsample/timeout_test.go
+++ httpsample/timeout_test.go
@@ -1,6 +1,32 @@
 package httpsample_test
 
-import "testing"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
-func TestTimeout(t *testing.T) {
+func TestSimple200(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("content-length", "5")
+		w.Header().Set("content-type", "text/plain")
+		w.WriteHeader(http.StatusOK)
+
+		w.Write([]byte("12345"))
+	}))
+	defer ts.Close()
+
+	resp, err := http.Get(ts.URL)
+	if err != nil {
+		t.Error("HTTP Get", err)
+	}
+	defer resp.Body.Close()
+
+	bs, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Error("ReadAll", err)
+	}
+	fmt.Printf("Body: %s\n", bs)
 }
