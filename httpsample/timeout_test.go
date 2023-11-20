745ac48f2103c91e2d7d72e565933c9119beebba
Shunichi Shinohara <shino.shun@gmail.com>
2023-11-20 15:57:16 +0900

    Use context

--- httpsample/timeout_test.go
+++ httpsample/timeout_test.go
@@ -1,6 +1,8 @@
 package httpsample_test
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"io"
 	"net"
@@ -123,6 +125,76 @@ func TestTimeoutAtBodyRead(t *testing.T) {
 			t.Logf("err.Temporary(): %#v", v.Temporary())
 		default:
 			// This clause is executed, err is not net.Error instance
+		}
+		return
+	}
+
+	// var b bytes.Buffer
+	// count, err := io.CopyN(&b, resp.Body, 5)
+	// fmt.Printf("count: %#v\n", count) // count = 3
+	// fmt.Printf("err: %#v\n", err)
+	// count, err = io.CopyN(&b, resp.Body, 2)
+	// fmt.Printf("count: %#v\n", count)
+	// fmt.Printf("err: %#v\n", err)     // unexpected EOF
+
+	t.Fatal("Must not reach here")
+}
+
+func TestTimeoutAtBodyReadWithContext(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(500 * time.Millisecond)
+		w.Header().Set("content-length", "5")
+		w.Header().Set("content-type", "text/plain")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("123"))
+		go func() {
+			time.Sleep(2000 * time.Millisecond)
+			w.Write([]byte("45"))
+		}()
+	}))
+	defer ts.Close()
+
+	c := &http.Client{
+		Timeout: 2000 * time.Millisecond,
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 1000*time.Millisecond)
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, ts.URL, nil)
+	if err != nil {
+		t.Logf("err: %v", err)
+		t.Logf("err: %#v", err)
+		t.Fatal("HTTP NewRequestWithContext", err)
+	}
+	t.Log("About to Get")
+	resp, err := c.Do(req)
+	if err != nil {
+		t.Logf("err: %v", err)
+		t.Logf("err: %#v", err)
+		t.Fatal("HTTP Get", err)
+	}
+	t.Logf("resp.Status: %#v\n", resp.Status)
+
+	defer resp.Body.Close()
+
+	if _, err := io.ReadAll(resp.Body); err != nil {
+		// Outputs are at Go 1.12.3
+		t.Logf("c.Get err")
+		// Output: unexpected EOF
+		t.Logf("err: %v", err)
+		// Check the context
+		select {
+		case <-ctx.Done():
+			t.Log("context done")
+			// Output: context deadline exceeded
+			t.Logf("context err: %+v", ctx.Err())
+			// Output: true
+			t.Logf("ctx.Err() is context.DeadlineExceeded: %v", errors.Is(ctx.Err(), context.DeadlineExceeded))
+			switch v := ctx.Err().(type) {
+			case net.Error:
+				// Output: true
+				t.Logf("timeout:   %v", v.Timeout())
+				// Output: true     *Deprecated*
+				t.Logf("temporary: %v", v.Temporary())
+			}
 		}
 		return
 	}
