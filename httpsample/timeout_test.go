cea51ca73223e6a62c1a60889041aa6872628b75
Shunichi Shinohara <shino.shun@gmail.com>
2023-11-20 09:57:31 +0900

    simple timeout case

--- httpsample/timeout_test.go
+++ httpsample/timeout_test.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 func TestSimple200(t *testing.T) {
@@ -18,10 +19,46 @@ func TestSimple200(t *testing.T) {
 	}))
 	defer ts.Close()
 
-	resp, err := http.Get(ts.URL)
+	c := &http.Client{
+		Timeout: 3000 * time.Microsecond,
+	}
+	resp, err := c.Get(ts.URL)
 	if err != nil {
 		t.Error("HTTP Get", err)
 	}
+	fmt.Printf("resp.Status: %#v\n", resp.Status)
+	fmt.Printf("resp.Body: %#v\n", resp.Body)
+
+	defer resp.Body.Close()
+
+	bs, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Error("ReadAll", err)
+	}
+	fmt.Printf("Body: %s\n", bs)
+}
+
+func TestTimeoutBeforeResponse(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(500 * time.Millisecond)
+		w.Header().Set("content-length", "5")
+		w.Header().Set("content-type", "text/plain")
+		w.WriteHeader(http.StatusOK)
+
+		w.Write([]byte("12345"))
+	}))
+	defer ts.Close()
+
+	c := &http.Client{
+		Timeout: 300 * time.Microsecond,
+	}
+	resp, err := c.Get(ts.URL)
+	if err != nil {
+		t.Error("HTTP Get", err)
+	}
+	fmt.Printf("resp.Status: %#v\n", resp.Status)
+	fmt.Printf("resp.Body: %#v\n", resp.Body)
+
 	defer resp.Body.Close()
 
 	bs, err := io.ReadAll(resp.Body)
